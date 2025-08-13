0de40f52c9d5e9f0578e02de65781b38e11f9ac9
giulia <134700744+giuliaalvess@users.noreply.github.com>
2025-08-13 10:07:47 -0300

    tentativa

--- geometrias/espacial.java
+++ geometrias/espacial.java
@@ -1,5 +1,13 @@
-package geometrias;
+import java.util.Scanner;
+import java.util.*;
+import static java.lang.Math.PI;
 
-public class espacial {
-    
+
+
+class esfera {
+    double PI, raio, areaSuperficial; //soma, sub;
+    areaSuperficial = 4 * PI * (Math.pow(raio, 2));
+     //4 pi r^2
+    volume = (4 / 3) * PI * (Math.pow(raio, 3));
 }
+
