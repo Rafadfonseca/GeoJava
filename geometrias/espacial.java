ed3c0667792312d71f3ce1d45608ba1a2fd1dc97
giulia <134700744+giuliaalvess@users.noreply.github.com>
2025-08-13 10:15:58 -0300

    esfera

--- geometrias/espacial.java
+++ geometrias/espacial.java
@@ -1,13 +1,29 @@
-import java.util.Scanner;
+package geometrias;
+
 import java.util.*;
-import static java.lang.Math.PI;
+import static java.lang.Math.*;
 
+public class espacial {
 
+    public float esfera() {
+        double PI, raio, areaSuperficial, volume; //soma, sub;
+        areaSuperficial = 4 * Math.PI * (Math.pow(raio, 2));
+        //4 pi r^2
+        volume = (4 / 3) * Math.PI * (Math.pow(raio, 3));
+        
+        if( operacao == 1){
+            return volume;
+        }else{
+            return areaSuperficial;
+        }
+    }
 
-class esfera {
-    double PI, raio, areaSuperficial; //soma, sub;
-    areaSuperficial = 4 * PI * (Math.pow(raio, 2));
-     //4 pi r^2
-    volume = (4 / 3) * PI * (Math.pow(raio, 3));
+    public float cilindro(){
+
+    }
+
 }
+
+
+
 
